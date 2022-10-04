a20cdfecdcd1283b2ec366dd5f1da6ffa4ca6b0d
Nickolas Orellana <p23norellanagonzalez@fairfieldprep.org>
2022-10-04 09:11:44 -0400

    Work from alternative computer for labs 3-5 and 3-6

---
+++ Lab_3-5.py
@@ -0,0 +1,14 @@
+#author NDO 10/03/22
+import time 
+import math 
+
+x = math.pow(2,2)
+print(x)
+
+y = time.process_time()
+print(y)
+# time.process_time is 0.033891 and changing 
+
+z = time.perf_counter()
+print(z) 
+# the time c
---
+++ Lab_3-6.py
@@ -0,0 +1,21 @@
+# author NDO 10/03/22
+
+var_string = 'Fairfield '
+var_two = 'Prep'
+print(var_string + var_two)
+
+var_string = 'I like Fairfield Prep '
+print(var_string * 20)
+
+var_statement = 'I\'m Loving this short story I\'m reading, "The Fall of the House of Usher"'
+print(var_statement)
+
+find_length = len('I\'m Loving this short story I\'m reading, "The Fall of the House of Usher"')
+print(find_length)
+
+var_word = 'apple'
+print(var_word[3]) 
+
+
+
+
